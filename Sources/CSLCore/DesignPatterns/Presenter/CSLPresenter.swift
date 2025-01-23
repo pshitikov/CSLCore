fcfdd467369e3fc2fd0d90912244ed3926f42f4d
lomanoff <49125179+lomanoff@users.noreply.github.com>
2025-01-23 14:25:24 +0500

    Update CSLPresenter.swift (#1)
    * Update CSLPresenter.swift
    
    * Update CSLPresenter.swift

--- Sources/CSLCore/DesignPatterns/Presenter/CSLPresenter.swift
+++ Sources/CSLCore/DesignPatterns/Presenter/CSLPresenter.swift
@@ -3,6 +3,24 @@ import SwiftUI
 @Observable
 public final class CSLPresenter<T: Identifiable> {
     
-    var model: T?
-    var isPresented = false
+    // MARK: - Static Properties
+    
+    public static var emptyModel: CSLPresenter<CSLAnyIdentifiable> { .init(model: .init()) }
+    
+    // MARK: - Observable
+    
+    public var model: T?
+    public var isPresented: Bool
+    
+    // MARK: - Initialization
+    
+    public init(model: T? = nil) {
+        self.model = model
+        
+        isPresented = false
+    }
+
+    // MARK: - Public Functions
+    
+    public func present() { isPresented = true }
 }
